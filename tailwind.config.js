fb6e06d1396b33d7c44c854dc6d7616edc188bff
sdphat <77732077+sdphat@users.noreply.github.com>
2023-05-24 19:06:23 +0700

    Setup color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,23 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {},
+  theme: {
+    colors: {
+      primary: '#0088FF',
+      'primary-dark': '#0066CC',
+      'primary-light': '#66B3FF',
+      secondary: '#FFFFFF',
+      'secondary-dark': '#E5E5E5',
+      accent: '#FFC107',
+      'accent-dark': '#FFA000',
+      'accent-light': '#FFD54F',
+      background: '#F9F9F9',
+      'background-dark': '#EDEDED',
+      'background-light': '#FFFFFF',
+      text: '#333333',
+      'text-dark': '#222222',
+      'text-light': '#666666',
+    },
+  },
   plugins: [],
-}
+};
