d0143b96fe6f113db09e0ac069fab9aa807d177c
sdphat <77732077+sdphat@users.noreply.github.com>
2023-10-02 13:37:01 +0700

    Reroute user to login page when root url is typed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,10 @@
+'use client';
+
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
-  return (
-    <article className="prose">
-      <button className="btn btn-primary">Primary button</button>
-      <button className="btn btn-secondary">Secondary button</button>
-      <button className="btn btn-accent">Secondary button</button>
-      <h1>Garlic bread with cheese: What the science tells us</h1>
-      <p>
-        For years parents have espoused the health benefits of eating garlic bread with cheese to
-        their children, with the food earning such an iconic status in our culture that kids will
-        often dress up as warm, cheesy loaf for Halloween.
-      </p>
-      <p>
-        But a recent study shows that the celebrated appetizer may be linked to a series of rabies
-        cases springing up around the country.
-      </p>
-    </article>
-  );
+  const router = useRouter();
+  router.push('/login');
+  return <></>;
 }
