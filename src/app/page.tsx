02caa196330df143302904a0f6ef462f119eb3f0
sdphat <77732077+sdphat@users.noreply.github.com>
2023-10-16 13:59:43 +0700

    Fix routing problem which causes building process to crash

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,12 @@
 
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Home() {
   const router = useRouter();
-  router.push('/login');
+  useEffect(() => {
+    router.push('/login');
+  }, []);
   return <></>;
 }
